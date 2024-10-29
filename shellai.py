e5f12b8897763ff40df0c1851bd996e7293db0ff
Elijah <ElijahKnaperek@gmail.com>
2024-10-28 23:43:42 -0400

    remove old imports

--- shellai.py
+++ shellai.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-from google.auth import default
 import litellm
 from litellm.types.utils import ModelResponse
 from typing import cast
 import requests
-import json
 import sys
 import os
 import subprocess
